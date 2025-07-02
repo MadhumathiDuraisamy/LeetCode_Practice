54923eaa1ba3bdd51956371c707d30b43cc7cae8
Madhumathi D <madhumathi1705@gmail.com>
2025-07-02 23:51:30 +0530

    Time: 2 ms (97.28%), Space: 45.4 MB (57.89%) - LeetHub

--- 2398-check-if-matrix-is-x-matrix/2398-check-if-matrix-is-x-matrix.java
+++ 2398-check-if-matrix-is-x-matrix/2398-check-if-matrix-is-x-matrix.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean checkXMatrix(int[][] grid) {
-        int n = grid.length;
-
-        for (int i = 0; i < n; i++) {
-            if (grid[i].length != n) return false; // ensure square matrix
-
-            for (int j = 0; j < n; j++) {
-                boolean isPrimary = (i == j);
-                boolean isSecondary = (i + j == n - 1);
-
-                if (isPrimary || isSecondary) {
-                    if (grid[i][j] == 0) return false; // diagonals must be non-zero
-                } else {
-                    if (grid[i][j] != 0) return false; // non-diagonals must be zero
+          for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (i == j || i == grid.length - 1 - j) {
+                    if (grid[i][j] == 0) {
+                        return false;
+                    }
+                } else if (grid[i][j] != 0) {
+                    return false;
                 }
             }
         }
