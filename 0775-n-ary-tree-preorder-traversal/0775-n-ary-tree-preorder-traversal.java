68ec20ca4558962680403f8d1595a0421a19d23f
Madhumathi D <madhumathi1705@gmail.com>
2025-07-25 14:25:34 +0530

    Time: 0 ms (100%), Space: 44.9 MB (29.48%) - LeetHub

--- 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.java
+++ 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.java
@@ -19,18 +19,28 @@ class Node {
 
 class Solution {
     public List<Integer> preorder(Node root) {
-        Stack<Node> st=new Stack<>();
-        ArrayList<Integer> res=new ArrayList<>();
-        if(root==null) return res;
-        st.push(root);
-        while(!st.isEmpty()){
-            Node temp=st.pop();
-            res.add(temp.val);
-            for(int i=temp.children.size()-1;i>=0;i--){
-                Node t=temp.children.get(i);
-                st.push(t);
-            }
-        }
+        // Stack<Node> st=new Stack<>();
+        // ArrayList<Integer> res=new ArrayList<>();
+        // if(root==null) return res;
+        // st.push(root);
+        // while(!st.isEmpty()){
+        //     Node temp=st.pop();
+        //     res.add(temp.val);
+        //     for(int i=temp.children.size()-1;i>=0;i--){
+        //         Node t=temp.children.get(i);
+        //         st.push(t);
+        //     }
+        // }
+        // return res;
+        List<Integer> res=new ArrayList<>();
+        preorder(root,res);
         return res;
+    }
+    public void preorder(Node root,List<Integer> res){
+        if(root==null) return;
+        res.add(root.val);
+        for(Node child:root.children){
+            preorder(child,res);
+        }
     }
 }
