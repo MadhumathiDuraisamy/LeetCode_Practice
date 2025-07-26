ce30e807c062d5f64b88b45bb3ff6846dc283f85
Madhumathi D <madhumathi1705@gmail.com>
2025-07-26 11:57:37 +0530

    Time: 2 ms (99.27%), Space: 54.7 MB (7.51%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,23 +1,38 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(n<1) return -1;
+        // if(n<1) return -1;
+        // if(n==1) return 1;
+        // ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        // for(int i=0;i<=n;i++) list.add(new ArrayList<>());
+        // for(int i=0;i<trust.length;i++){
+        //     int u=trust[i][0];
+        //     int v=trust[i][1];
+        //     list.get(u).add(v);
+        // }
+        // for(int i=1;i<=n;i++){
+        //     if(list.get(i).size()==0){
+        //         int cnt=0;
+        //         for(int j=1;j<=n;j++){
+        //             if(list.get(j).contains(i)) cnt++;
+        //         }
+        //         if(cnt==n-1) return i;
+        //     }
+        // }
+        // return -1; 
         if(n==1) return 1;
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-        for(int i=0;i<=n;i++) list.add(new ArrayList<>());
-        for(int i=0;i<trust.length;i++){
-            int u=trust[i][0];
-            int v=trust[i][1];
-            list.get(u).add(v);
+        int judge[]=new int[n+1];
+        for(int[]a:trust)
+        { 
+            judge[a[0]]--;
+            judge[a[1]]++;
         }
-        for(int i=1;i<=n;i++){
-            if(list.get(i).size()==0){
-                int cnt=0;
-                for(int j=1;j<=n;j++){
-                    if(list.get(j).contains(i)) cnt++;
-                }
-                if(cnt==n-1) return i;
-            }
+        for(int i=0;i<=n;i++)
+        {
+             if(judge[i]==n-1)
+             { 
+                return i;
+             }
         }
-        return -1;  
+        return -1; 
     }
 }
