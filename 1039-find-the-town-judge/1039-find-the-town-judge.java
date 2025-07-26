23e658d9c03f6fe06ed19f4f5b526fa4d5de3737
Madhumathi D <madhumathi1705@gmail.com>
2025-07-26 11:56:00 +0530

    Time: 7 ms (15.58%), Space: 52.1 MB (93.93%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(n<1) return -1;
+        if(n==1) return 1;
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        for(int i=0;i<=n;i++) list.add(new ArrayList<>());
+        for(int i=0;i<trust.length;i++){
+            int u=trust[i][0];
+            int v=trust[i][1];
+            list.get(u).add(v);
+        }
+        for(int i=1;i<=n;i++){
+            if(list.get(i).size()==0){
+                int cnt=0;
+                for(int j=1;j<=n;j++){
+                    if(list.get(j).contains(i)) cnt++;
+                }
+                if(cnt==n-1) return i;
+            }
+        }
+        return -1;  
+    }
+}
